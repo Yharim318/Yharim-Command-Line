dfed68013d0146dc7d4b4adddb8e915c836078b5
Yharim318 <gradylikavec@gmail.com>
2024-03-29 14:18:40 -0400

    Release Commit

--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10525e1083a1bc894019d39bb90394d09c6b5365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6733fa48ee443e04317adeb528a42acd2eb6c7cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
