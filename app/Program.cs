bb0a752c30441f26d7fea8f1868498ccb0184cc3
Yharim318 <gradylikavec@gmail.com>
2024-03-29 12:33:58 -0400

    first commit

---
+++ app/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Welcome to Yharim's Command Line!");
+Console.WriteLine("----------------------------------");
+Kill("Your Mother");
+Console.ReadKey();
+
+void Kill(string name) => Console.WriteLine($"{name} is dead now!");
